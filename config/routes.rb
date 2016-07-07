41692d717f9b9668e05ed2c454fad88dca91ff4c
Matt <von_m@hotmail.co.uk>
2016-07-07 14:07:15 +0000

    added random number page

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   get 'pages/contact'
 
   get 'pages/about'
+  
+  get 'pages/random'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
